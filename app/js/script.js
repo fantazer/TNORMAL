7122b23c397ca46e1b6fc42a446acc77cd65a205
FANTAZER <kuznecovs45@gmail.com>
2018-10-01 20:11:10 +0300

    add modal + slick + some fixes

--- app/js/script.js
+++ app/js/script.js
@@ -1,58 +1,110 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+	//modal
+	var modalState = {
+		"isModalShow": false, //state show modal
+		"scrollPos": 0
+	};
+	$('.modal-content').click(function (event) {
+		event.stopPropagation();
+	});
 
-	/* ###### For only ies  ######*/
-	//if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)){
-	//	//code
-	//}
+	var openModal = function () {
+		if (!$('.modal-layer').hasClass('modal-layer-show')) {
+			$('.modal-layer').addClass('modal-layer-show');
+			modalState.scrollPos = $(window).scrollTop();
+			$('body').css({
+				overflow: 'hidden',
+				position: 'fixed',
+				overflowY: 'scroll',
+				top: -modalState.scrollPos,
+				width: '100%'
+			});
+		}
+		modalState.isModalShow = true;
+	};
+
+	var closeModal = function () {
+		$('.modal-layer').removeClass('modal-layer-show');
+		$('body').css({
+			overflow: '',
+			position: '',
+			top: modalState.scrollPos
+		});
+		$(window).scrollTop(modalState.scrollPos);
+		$('.modal').removeClass('modal__show');
+		modalState.isModalShow = false;
+	};
+
+	var initModal = function (el) {
+		openModal();
+		$('.modal').each(function () {
+			if ($(this).data('modal') === el) {
+				$(this).addClass('modal__show')
+			} else {
+				$(this).removeClass('modal__show')
+			}
+		});
+		var modalHeightCont = $(window).height();
+		$('.modal-filter').height(modalHeightCont);
+
+	};
+
+	$('.modal-get').click(function () {
+		var currentModal = $(this).data("modal");
+		initModal(currentModal);
+	});
+
+	$('.modal-layer , .modal-close').click(function () {
+		closeModal();
+	});
+	//modals===end
 
 	function detectIE() {
-	var ua = window.navigator.userAgent;
+		var ua = window.navigator.userAgent;
 
-	var msie = ua.indexOf('MSIE ');
-	if (msie > 0) {
-		// IE 10 or older => return version number
-		return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+		var msie = ua.indexOf('MSIE ');
+		if (msie > 0) {
+			// IE 10 or older => return version number
+			return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+		}
+
+		var trident = ua.indexOf('Trident/');
+		if (trident > 0) {
+			// IE 11 => return version number
+			var rv = ua.indexOf('rv:');
+			return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+		}
+
+		var edge = ua.indexOf('Edge/');
+		if (edge > 0) {
+			// Edge (IE 12+) => return version number
+			return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+		}
+
+		// other browser
+		return false;
 	}
 
-	var trident = ua.indexOf('Trident/');
-	if (trident > 0) {
-		// IE 11 => return version number
-		var rv = ua.indexOf('rv:');
-		return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-	}
-
-	var edge = ua.indexOf('Edge/');
-	if (edge > 0) {
-		// Edge (IE 12+) => return version number
-		return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-	}
-
-	// other browser
-	return false;
-}
-
-	console.log(detectIE());
 	if (detectIE() <= 14 && detectIE()) {
 		$('body').empty();
 		$('body').prepend('' +
-		 '<div class="old-browser">' +
+			'<div class="old-browser">' +
 			'<div class="old-browser-text"> Сайт не поддерживае Браузер Internet Explorer</div><br>' +
 			'<div class="old-browser-text"> Установите <br><br> Chrome Firefox Opera </div><br>' +
-		'</div>');
+			'</div>');
 	}
+	//for init SVG
 
-	//for init SVG 
-	svg4everybody();
 	// ==== clear storage =====
-	 localStorage.clear();
-	 sessionStorage.clear();
-	 $(window).unload(function(){
-		 localStorage.clear();
-	 });
+	localStorage.clear();
+	sessionStorage.clear();
+	$(window).unload(function () {
+		localStorage.clear();
+	});
 	// ==== clear storage end =====
 
-	
+
 	/* ###### For SlideToggle Elements  ######*/
 	/*var hideToggle = function(targetClick,toggleEl) {
 		$(targetClick).click(function(event){
@@ -72,58 +124,50 @@ $(document).ready(function(){
 
 //cash SVG
 
-;( function( window, document )
-{
+;(function (window, document) {
 	'use strict';
 
-	var file  = 'img/pack.html',
+	var file = 'img/pack.html',
 		revision = 1;
 
-	if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
+	if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect)
 		return true;
 
-	var isLocalStorage = 'localStorage' in window && window[ 'localStorage' ] !== null,
+	var isLocalStorage = 'localStorage' in window && window['localStorage'] !== null,
 		request,
 		data,
-		insertIT = function()
-		{
-			document.body.insertAdjacentHTML( 'afterbegin', data );
+		insertIT = function () {
+			document.body.insertAdjacentHTML('afterbegin', data);
 		},
-		insert = function()
-		{
-			if( document.body ) insertIT();
-			else document.addEventListener( 'DOMContentLoaded', insertIT );
+		insert = function () {
+			if (document.body) insertIT();
+			else document.addEventListener('DOMContentLoaded', insertIT);
 		};
 
-	if( isLocalStorage && localStorage.getItem( 'inlineSVGrev' ) == revision )
-	{
-		data = localStorage.getItem( 'inlineSVGdata' );
-		if( data )
-		{
+	if (isLocalStorage && localStorage.getItem('inlineSVGrev') == revision) {
+		data = localStorage.getItem('inlineSVGdata');
+		if (data) {
 			insert();
 			return true;
 		}
 	}
 
-	try
-	{
+	try {
 		request = new XMLHttpRequest();
-		request.open( 'GET', file, true );
-		request.onload = function()
-		{
-			if( request.status >= 200 && request.status < 400 )
-			{
+		request.open('GET', file, true);
+		request.onload = function () {
+			if (request.status >= 200 && request.status < 400) {
 				data = request.responseText;
 				insert();
-				if( isLocalStorage )
-				{
-					localStorage.setItem( 'inlineSVGdata',  data );
-					localStorage.setItem( 'inlineSVGrev',   revision );
+				if (isLocalStorage) {
+					localStorage.setItem('inlineSVGdata', data);
+					localStorage.setItem('inlineSVGrev', revision);
 				}
 			}
 		}
 		request.send();
 	}
-	catch( e ){}
+	catch (e) {
+	}
 
-}( window, document ) );
+}(window, document));
