58c8d631344b30929be14c9d0ec848202c9e671e
FANTAZER <kuznecovs45@gmail.com>
2020-05-13 15:59:11 +0300

    fix location fonts + scripts

--- app/js/script.js
+++ app/js/script.js
@@ -1,29 +1,32 @@
 $(document).ready(function () {
 
-// nice select
-//$('.select-beauty').niceSelect();
-// nice select === end
+	// nice select
+	//$('.select-beauty').niceSelect();
+	// nice select === end
 
 
-// fix top-menu
-var shrinkHeader = 250;
-var head = $('.header');
-var heightHeader = head.height();
-$(window).scroll(function() {
-	var scroll = $(this).scrollTop();
-	if ( scroll >= shrinkHeader ) {
-			$('body').css('paddingTop',heightHeader);
-			head.addClass('shrink');
-		}
-		else {
-				$('body').css('paddingTop',0);
-				head.removeClass('shrink');
-		}
-});
+	// fix top-menu
+	var shrinkHeader = 250;
+	var head = $('.header');
+	var heightHeader = head.height();
+	$(window).scroll(function() {
+		var scroll = $(this).scrollTop();
+		if ( scroll >= shrinkHeader ) {
+				$('body').css('paddingTop',heightHeader);
+				head.addClass('shrink');
+			}
+			else {
+					$('body').css('paddingTop',0);
+					head.removeClass('shrink');
+			}
+	});
 
-$(window).resize(function(){
-	heightHeader=head.height();
-});
-// fix top-menu === end
+	$(window).resize(function(){
+		heightHeader=head.height();
+	});
+	// fix top-menu === end
 
+
+	window.condition = {};
+	window.condition.info = info;
 });
