01a5afa75d8ceb32316cfeac0f7837935a5e8750
FANTAZER <kuznecovs45@gmail.com>
2020-05-27 14:49:00 +0300

    add version 3.0.1

--- app/js/script.js
+++ app/js/script.js
@@ -4,6 +4,68 @@ $(document).ready(function () {
 	//$('.select-beauty').niceSelect();
 	// nice select === end
 
+	//modals
+	var modalState = {
+		"isModalShow": false, //state show modal
+		"scrollPos": 0
+	};
+	var scrollWidth= window.innerWidth - $(document).width();
+	var openModal = function () {
+		if (!$('.modal-layer').hasClass('modal-layer-show')) {
+			$('.modal-layer').addClass('modal-layer-show');
+			modalState.scrollPos = $(window).scrollTop();
+			$('body').css({
+				overflowY: 'hidden',
+				top: -modalState.scrollPos,
+				width: '100%',
+				paddingRight:scrollWidth
+			});
+
+		}
+		modalState.isModalShow = true;
+	};
+
+	var closeModal = function () {
+		$('.modal-layer').removeClass('modal-layer-show');
+		$('body').css({
+			overflow: '',
+			position: '',
+			top: modalState.scrollPos,
+			paddingRight:0
+		});
+		$(window).scrollTop(modalState.scrollPos);
+		$('.modal').addClass('modal-hide-animation');
+		setTimeout(function(){
+			$('.modal').removeClass('modal-hide-animation');
+			$('.modal').removeClass('modal__show');
+		},600);
+		modalState.isModalShow = false;
+	};
+
+	var initModal = function (el) {
+		openModal();
+
+		$('.modal').each(function () {
+			if ($(this).data('modal') === el) {
+				$(this).addClass('modal__show')
+			} else {
+				$(this).removeClass('modal__show')
+			}
+		});
+		var modalHeightCont = $(window).height();
+		$('.modal-filter').height(modalHeightCont);
+
+	};
+
+	$('.modal-get').click(function () {
+		var currentModal = $(this).data("modal");
+		initModal(currentModal);
+	});
+
+	$('.modal-close, .modal-hide').click(function () {
+		closeModal();
+	});
+	//modals===end
 
 	// fix top-menu
 	var shrinkHeader = 250;
---
+++ dist/js/add-on.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+	function detectIE() {
+		var ua = window.navigator.userAgent;
+
+		var msie = ua.indexOf('MSIE ');
+		if (msie > 0) {
+			// IE 10 or older => return version number
+			return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+		}
+
+		var trident = ua.indexOf('Trident/');
+		if (trident > 0) {
+			// IE 11 => return version number
+			var rv = ua.indexOf('rv:');
+			return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+		}
+
+		var edge = ua.indexOf('Edge/');
+		if (edge > 0) {
+			// Edge (IE 12+) => return version number
+			return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+		}
+
+		// other browser
+		return false;
+	}
+	if (detectIE() <= 14 && detectIE()) {
+		$('body').empty();
+		$('body').prepend('' +
+			'<div class="old-browser">' +
+			'<div class="old-browser-text"> Сайт не поддерживае Браузер Internet Explorer</div><br>' +
+			'<div class="old-browser-text"> Установите <br><br> Chrome Firefox Opera </div><br>' +
+			'</div>');
+
+	}
+	// ==== clear storage =====
+	localStorage.clear();
+	sessionStorage.clear();
+	$(window).unload(function () {
+		localStorage.clear();
+	});
+	// ==== clear storage end =====
+
+	/* ###### For SlideToggle Elements  ######*/
+	/*var hideToggle = function(targetClick,toggleEl) {
+		$(targetClick).click(function(event){
+				event.stopPropagation();
+				$(toggleEl).slideToggle("fast");
+		});
+		$(toggleEl).on("click", function (event) {
+			event.stopPropagation();
+		});
+		$(document).on("click", function () {
+				$(toggleEl).hide();
+		});
+	}
+	hideToggle('.icon-bars','.top-menu_link');*/
+})
+
+//for init SVG
+;(function (window, document) {
+	'use strict';
+
+	var file = 'img/pack.html',
+		revision = 1;
+
+	if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect)
+		return true;
+
+	var isLocalStorage = 'localStorage' in window && window['localStorage'] !== null,
+		request,
+		data,
+		insertIT = function () {
+			document.body.insertAdjacentHTML('afterbegin', data);
+		},
+		insert = function () {
+			if (document.body) insertIT();
+			else document.addEventListener('DOMContentLoaded', insertIT);
+		};
+
+	if (isLocalStorage && localStorage.getItem('inlineSVGrev') == revision) {
+		data = localStorage.getItem('inlineSVGdata');
+		if (data) {
+			insert();
+			return true;
+		}
+	}
+
+	try {
+		request = new XMLHttpRequest();
+		request.open('GET', file, true);
+		request.onload = function () {
+			if (request.status >= 200 && request.status < 400) {
+				data = request.responseText;
+				insert();
+				if (isLocalStorage) {
+					localStorage.setItem('inlineSVGdata', data);
+					localStorage.setItem('inlineSVGrev', revision);
+				}
+			}
+		}
+		request.send();
+	} catch (e) {
+	}
+
+}(window, document));
