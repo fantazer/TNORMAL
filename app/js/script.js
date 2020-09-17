bb51f45cd1726215a82b4155c90f2d32c49340c8
FANTAZER <kuznecovs45@gmail.com>
2020-09-17 19:48:02 +0300

    add cookie msg

--- app/js/script.js
+++ app/js/script.js
@@ -180,6 +180,31 @@ $(document).ready(function () {
 	});
 	// dropdown === end
 
+	//bubble
+	var limit = 2400 * 3600 * 1000; // 24 часа
+	var localStorageInitTime = localStorage.getItem('localStorageInitTime');
+	//console.log(localStorageInitTime);
+	//console.log(+new Date() - localStorageInitTime);
+	if (localStorageInitTime === null) {
+			localStorage.setItem('localStorageInitTime', +new Date());
+	} else if(+new Date() - localStorageInitTime > limit){
+			localStorage.setItem('localStorageInitTime', +new Date());
+			localStorage.setItem('bubble', '1');
+	};
+
+	if(localStorage.getItem('bubble')!='0'){
+		setTimeout(function(){
+			$('.cookie').addClass('cookie--active');
+			//console.log('111');
+		},3000);
+	}
+
+	$('.cookie .icon-close').click(function(){
+		$(this).closest('.cookie').removeClass('cookie--active');
+		localStorage.setItem('bubble', '0');
+	});
+	//bubble===end
+
 	//window.condition = {};
 	//window.condition.info = info;
 });
